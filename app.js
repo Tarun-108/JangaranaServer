f067abe7b6c4072e9ee85ddd62d2fbf5851c0f5b
Tarun-108 <tarunstu125@gmail.com>
2022-05-09 17:37:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
+
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+require("dotenv").config();
+
+
+//DB
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=>{
+    console.log("DB Connected")
+}).catch(()=>{
+    console.log("Unable to connect to DB")
+})
+
+//Use parsing middleware
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+//Importing routes
+const userRoutes = require("./routes/user")
+
+//Using routes
+app.use('/api/auth', userRoutes)
+
+const port = process.env.PORT || 8000
+
+//Starting a server
+app.listen(port,()=>{
+    console.log(`App is running at ${port}`)
+})
